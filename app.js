e839265c7bfc908e3632e1ff5f805248853c1acd
sumruti <sumitchoudhary727@gmail.com>
2019-10-04 20:20:43 +0530

    first commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'asset')));
-const port = process.env.PORT || 5000;
+
 // Jade
 app.set('views', __dirname+'/views');
 app.set('view engine', 'jade');
@@ -13,5 +13,17 @@ app.get('/', function(req, res){
 	res.render('index');
 });
 
-app.listen(port, () => console.log("Server started on port " + port));
+app.get('/about-us', function(req, res){
+	res.render('about_us');
+});
+
+app.get('/items-available', function(req, res){
+	res.render('items_available');
+});
+
+app.get('/contact-us', function(req, res){
+	res.render('contact_us');
+});
+
+app.listen(3333);
 console.log('Running on port 3333!');
